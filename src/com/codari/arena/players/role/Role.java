f1c361617b4266ac5a993defb097449e10205b05
Diage <farina.1919@gmail.com>
2013-11-09 14:35:12 -0500

    Forgot to send the role too!

---
+++ src/com/codari/arena/players/role/Role.java
@@ -0,0 +1,18 @@
+package com.codari.arena.players.role;
+
+import com.codari.arena.players.Skill;
+
+/**
+ * A role identifies core characteristics about how the player will interact within the game. 
+ * 	These roles can be used to define skills and spells players have the ability to use or interact with.
+ * @author Ryan
+ *
+ */
+public interface Role {
+	
+	/**
+	 * Retrieves the skill for this role. 
+	 * @return The {@link Skill} for this player. 
+	 */
+	public Skill getSkill();
+}
