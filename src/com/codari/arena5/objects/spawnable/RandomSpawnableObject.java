c4711ff0277419fc9a574b32e24d75f3702615f1
Diage <farina.1919@gmail.com>
2013-11-15 19:22:08 -0500

    Added weight to randomSpawnable.

--- src/com/codari/arena5/objects/spawnable/RandomSpawnableObject.java
+++ src/com/codari/arena5/objects/spawnable/RandomSpawnableObject.java
@@ -4,9 +4,17 @@ package com.codari.arena5.objects.spawnable;
  * 
  * These objects will be selected from a pool to be spawned given the spawning characteristics 
  * 	identified by the groupName.
+ * 
+ * Random spawnables have a weight which will determine the chance they spawn from the group they are in. 
+ * 	The smaller the weight the more rare. Note the effect of the weight is relative to the total weight in the groupName. 
  * @author Ryan
  *
  */
 public interface RandomSpawnableObject extends SpawnableObject {
-
+	
+	/**
+	 * Get the weight of this Random Spawnable Object
+	 * @return The weight of this RandomSpawnableObject, the higher the weight the less rare it is. 
+	 */
+	public int getWeight();
 }
