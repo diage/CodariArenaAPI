985d48dd4d65965b6a72858c4ef6d5553a4886a2
Soren025 <Soren11001@gmail.com>
2013-11-24 14:26:32 -0600

    ad

--- src/com/codari/arena5/rules/WinCondition.java
+++ src/com/codari/arena5/rules/WinCondition.java
@@ -1,6 +1,10 @@
 package com.codari.arena5.rules;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+
+import com.codari.api5.Codari;
 
 public abstract class WinCondition implements Listener {
 	//-----Fields-----//
@@ -9,5 +13,13 @@ public abstract class WinCondition implements Listener {
 	//-----Public Methods-----//
 	public final boolean conditionMet() {
 		return this.conditionMet;
+	}
+	
+	public final void setRegistered(boolean registered) {
+		if (registered) {
+			Bukkit.getPluginManager().registerEvents(this, Codari.INSTANCE);
+		} else {
+			HandlerList.unregisterAll(this);
+		}
 	}
 }
