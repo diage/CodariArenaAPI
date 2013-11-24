ea5cba499259d52820f81c0288db92c0125f0ca7
Soren025 <Soren11001@gmail.com>
2013-11-24 14:01:14 -0600

    asd

--- src/com/codari/arena5/rules/WinCondition.java
+++ src/com/codari/arena5/rules/WinCondition.java
@@ -4,14 +4,9 @@ import org.bukkit.event.Listener;
 
 public abstract class WinCondition implements Listener {
 	//-----Fields-----//
-	private boolean active;
 	protected boolean conditionMet;
 	
 	//-----Public Methods-----//
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	
 	public final boolean conditionMet() {
 		return this.conditionMet;
 	}
