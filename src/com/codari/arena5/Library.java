6a7389f89840c3fc951d5696816492fff1767762
Diage <farina.1919@gmail.com>
2013-12-08 16:18:06 -0500

    Made Library better! (More secure)

--- src/com/codari/arena5/Library.java
+++ src/com/codari/arena5/Library.java
@@ -1,22 +1,11 @@
 package com.codari.arena5;
 
-import org.bukkit.Location;
-
 import com.codari.arena5.objects.ArenaObject;
 import com.codari.arena5.rules.roledelegation.RoleDeclaration;
 import com.codari.arena5.rules.timedaction.TimedAction;
 import com.codari.arena5.rules.wincondition.WinCondition;
 
 public interface Library {
-
-	public WinCondition createWinCondition(String name, Location location);
-
-	public TimedAction createTimedAction(String name, Location location);
-
-	public RoleDeclaration createRoleDeclaration(String name, Location location);
-	
-	public ArenaObject createObject(String name, Location location);
-
 	public void registerWinCondition(Class<? extends WinCondition> clazz);
 
 	public void registerTimedAction(Class<? extends TimedAction> clazz);
