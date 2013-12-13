55fdb887f15a5fd12edd37661f15342660c0f958
Soren025 <Soren11001@gmail.com>
2013-12-12 23:04:33 -0600

    hotbar stuff no cancelly

--- src/com/codari/arena5/players/hotbar/HotbarSelectEvent.java
+++ src/com/codari/arena5/players/hotbar/HotbarSelectEvent.java
@@ -1,13 +1,12 @@
 package com.codari.arena5.players.hotbar;
 
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 
 import com.codari.arena5.players.combatants.Combatant;
 
-public class HotbarSelectEvent extends Event implements Cancellable {
+public class HotbarSelectEvent extends Event {
 	//-----Static Fields-----//
 	private static final HandlerList handlers = new HandlerList();
 	
@@ -20,7 +19,6 @@ public class HotbarSelectEvent extends Event implements Cancellable {
 	private final Combatant combatant;
 	private final HotbarSlot slot;
 	private final ItemStack item;
-	private boolean cancelled;
 	
 	//-----Constructor-----//
 	public HotbarSelectEvent(Combatant combatant, HotbarSlot slot, ItemStack item) {
@@ -45,15 +43,5 @@ public class HotbarSelectEvent extends Event implements Cancellable {
 	@Override
 	public HandlerList getHandlers() {
 		return handlers;
-	}
-
-	@Override
-	public boolean isCancelled() {
-		return this.cancelled;
-	}
-
-	@Override
-	public void setCancelled(boolean cancelled) {
-		this.cancelled = cancelled;
 	}
 }
