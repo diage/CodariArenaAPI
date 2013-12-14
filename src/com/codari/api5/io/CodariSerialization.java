63ad4fb8de91677849f8353434fa80c8ffee8e72
Soren025 <Soren11001@gmail.com>
2013-12-13 22:36:43 -0600

    asd

--- src/com/codari/api5/io/CodariSerialization.java
+++ src/com/codari/api5/io/CodariSerialization.java
@@ -9,9 +9,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.bukkit.configuration.serialization.ConfigurationSerializable;
-import org.bukkit.configuration.serialization.ConfigurationSerialization;
-import org.bukkit.configuration.serialization.SerializableAs;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 
@@ -20,9 +17,9 @@ public final class CodariSerialization {
 	private CodariSerialization(){}
 	
 	//-----Methods-----//
-	public static void serialize(Object obj, OutputStream out) throws CodariSerializationException {
+	public static void serialize(OutputStream out, Object obj) throws CodariSerializationException {
 		if (out == null) {
-			throw new IllegalArgumentException("OutputStream must not be null");
+			throw new NullPointerException();
 		}
 		try (BukkitObjectOutputStream objectOut = new BukkitObjectOutputStream(out)) {
 			objectOut.writeObject(obj);
@@ -31,9 +28,9 @@ public final class CodariSerialization {
 		}
 	}
 	
-	public static void serialize(Object obj, File file) throws CodariSerializationException {
+	public static void serialize(File file, Object obj) throws CodariSerializationException {
 		try (FileOutputStream fileOut = new FileOutputStream(file)) {
-			serialize(obj, fileOut);
+			serialize(fileOut, obj);
 		} catch (IOException ex) {
 			throw new CodariSerializationException(ex);
 		}
@@ -41,7 +38,7 @@ public final class CodariSerialization {
 	
 	public static byte[] serialize(Object obj) throws CodariSerializationException {
 		try (ByteArrayOutputStream byteDataOut = new ByteArrayOutputStream(512)) {
-			serialize(obj, byteDataOut);
+			serialize(byteDataOut, obj);
 			return byteDataOut.toByteArray();
 		} catch (IOException ex) {
 			throw new CodariSerializationException(ex);
@@ -50,7 +47,7 @@ public final class CodariSerialization {
 	
 	public static Object deserialize(InputStream in) throws CodariSerializationException {
 		if (in == null) {
-			throw new IllegalArgumentException("InputStream must not be null");
+			throw new NullPointerException();
 		}
 		try (BukkitObjectInputStream objectIn = new BukkitObjectInputStream(in)) {
 			return objectIn.readObject();
@@ -69,7 +66,7 @@ public final class CodariSerialization {
 	
 	public static Object deserialize(byte[] byteData) throws CodariSerializationException {
 		if (byteData == null) {
-			throw new IllegalArgumentException("Byte data must not be null");
+			throw new NullPointerException();
 		}
 		try(ByteArrayInputStream dataIn = new ByteArrayInputStream(byteData)) {
 			return deserialize(dataIn);
@@ -83,14 +80,5 @@ public final class CodariSerialization {
 		@SuppressWarnings("unchecked")//If it was serialized as T it should deserialize as T
 		T result = (T) deserialize(byteData);
 		return result;
-	}
-	
-	public static void registerConfigurationSerializable(Class<? extends ConfigurationSerializable> clazz) {
-		SerializableAs serializableAs = clazz.getAnnotation(SerializableAs.class);
-		if (serializableAs != null) {
-			ConfigurationSerialization.registerClass(clazz, serializableAs.value());
-		} else {
-			ConfigurationSerialization.registerClass(clazz);
-		}
 	}
 }
