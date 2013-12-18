bac1c4eb024f97f99906d67c58f2977401bd66f7
Soren025 <Soren11001@gmail.com>
2013-12-17 20:24:18 -0600

    enchantment stuff

--- src/com/codari/api5/enchantment/EnchantmentManager.java
+++ src/com/codari/api5/enchantment/EnchantmentManager.java
@@ -1,5 +1,8 @@
 package com.codari.api5.enchantment;
 
+import org.bukkit.inventory.ItemStack;
+
 public interface EnchantmentManager {
 	public void registerEnchantment(CustomEnchantment enchantment);
+	public boolean containsCustomEnchantment(ItemStack itemStack);
 }
