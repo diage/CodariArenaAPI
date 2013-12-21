12e11fd321d49abcbf42d171238a4f06cd941297
Diage <farina.1919@gmail.com>
2013-12-20 21:55:01 -0500

    Added more stuff to gamerule.

--- src/com/codari/arena5/arena/rules/GameRule.java
+++ src/com/codari/arena5/arena/rules/GameRule.java
@@ -7,7 +7,7 @@ import com.codari.api5.util.Time;
 import com.codari.arena5.arena.rules.roledelegation.RoleDeclaration;
 import com.codari.arena5.arena.rules.roledelegation.RoleDelegation;
 import com.codari.arena5.arena.rules.timedaction.TimedAction;
-import com.codari.arena5.arena.rules.wincondition.WinConditionTemplate;
+import com.codari.arena5.arena.rules.wincondition.WinCondition;
 
 import java.util.List;
 
@@ -20,18 +20,21 @@ import java.util.List;
  *
  */
 public interface GameRule extends Serializable {
+	
 	/**
 	 * Method to declare the valid team size which must be met before the game will accept a team. 
 	 * 
 	 * @param teamSize The size to set teams to for this game rule. 
 	 */
-	public void setTeamSize(int teamSize);
+	public void setTeamSize(byte teamSize); //DONE
+	
 	/**
 	 * Add a condition such that if it is ever met, the game will immediately end and declare victors.
 	 * 
 	 * @param winCondition The condition to be added.
 	 */
-	public void addWinCondition(WinConditionTemplate winCondition);
+	public void addWinCondition(WinCondition winCondition); //DONE - PARTIALLY
+	
 	/**
 	 * Add a win condition which is only valid before or after a certain point in time.
 	 * 
@@ -40,25 +43,29 @@ public interface GameRule extends Serializable {
 	 * @param after If this true, then the win condition will be valid after the time, otherwise it will be valid before the time. 
 	 * @return True if the win condition can possibly be met before the end of the game, false otherwise. 
 	 */
-	public boolean addWinCondition(WinConditionTemplate winCondition, Time time, boolean after);
+	public boolean addWinCondition(WinCondition winCondition, Time time, boolean after); //DONE - PARTIALLY
+	
 	/**
 	 * Removes the provided win condition from the current selection of win conditions.
 	 * 
 	 * @param winCondition The win condition to remove. 
 	 */
-	public void removeWinCondition(WinConditionTemplate winCondition);
+	public void removeWinCondition(WinCondition winCondition); //DONE - PARTIALLY
+	
 	/**
 	 * Set the duration of the match. Setting this more than once will overwrite the previous Match Duration.
 	 * 
 	 * @param time The time at which to end. 
 	 */
-	public void setMatchDuration(Time time);
+	public void setMatchDuration(Time time); //DONE
+	
 	/**
 	 * Set the duration of the match to be infinite. If a Time is already registered, it will be removed.
 	 * 
 	 * 	Registering a time after this will set it to that time. 
 	 */
-	public void setMatchDurationInfinite();
+	public void setMatchDurationInfinite(); //DONE
+	
 	/**
 	 * Add a passive action to happen along the time line of the arena. 
 	 * @param action The Timed Action to be performed. 
@@ -66,41 +73,42 @@ public interface GameRule extends Serializable {
 	 * 
 	 */
 	public boolean addAction(TimedAction action);
+	
 	/**
 	 * Check to see if a duration and at least one win condition have been set.
 	 * 	Only a valid Game Rule can be added to the arena.
 	 * 
 	 * @return True if the duration is set and at least one win condition exists, false otherwise. 
 	 */
-	public boolean isValid();
+	public boolean isValid(); //DO NOT CONSIDER
 	
 	/**
 	 * Method to get all of the {@link TimedAction}s for this GameRule
 	 * 
 	 * @return A collection of the TimedActions
 	 */
-	public Collection<TimedAction> getTimedActions();
+	public Collection<TimedAction> getTimedActions(); //DO NOT CONSIDER
 	
 	/**
-	 * Method to get all of the {@link WinConditionTemplate}s for this GameRule
+	 * Method to get all of the {@link WinCondition}s for this GameRule
 	 *  
 	 * @return A collection of WinConditions
 	 */
-	public Collection<WinConditionTemplate> getWinConditions();
+	public Collection<WinCondition> getWinConditions(); //DO NOT CONSIDER
 	
 	/**
 	 * Method to get the TeamSize that has been set for this GameRule.
 	 *   
 	 * @return The TeamSize for this GameRule
 	 */
-	public int getTeamSize();
+	public byte getTeamSize(); //DO NOT CONSIDER
 	
 	/**
 	 * Method to get the MatchDuration for this GameRule
 	 * 
 	 * @return The MatchDuration for this GameRule, for infinite length it will return a 0.
 	 */
-	public Time getMatchDuration();
+	public Time getMatchDuration(); //DO NOT CONSIDER
 	
 	/**
 	 * Method to add a {@link RoleDeclaration} to the Game Rule. One must be added for the arena to be valid. 
@@ -116,7 +124,8 @@ public interface GameRule extends Serializable {
 	 * 
 	 * @return The RoleDeclaration. 
 	 */
-	public List<RoleDeclaration> getRoleDeclaration();
+	public List<RoleDeclaration> getRoleDeclaration(); //DO NOT CONSIDER
+	
 	/**
 	 * Method to add a {@link RoleDelegation} to the Game Rule. One must be added for the arena to be valid. 
 	 * 
@@ -131,5 +140,9 @@ public interface GameRule extends Serializable {
 	 * 
 	 * @return The RoleDelegation. 
 	 */
-	public RoleDelegation getRoleDelegation();
+	public RoleDelegation getRoleDelegation(); //DO NOT CONSIDER
+	
+	public void setNumberOfTeams(byte numberOfTeams); //DONE
+	
+	public byte getNumberOfTeams(); //DO NOT CONSIDER
 }
