e969ee2ea9092ebd9ee92c1e5f02238ea05de818
Soren025 <Soren11001@gmail.com>
2014-01-15 17:22:41 -0600

    asdasd

--- src/com/codari/arena5/arena/rules/TokenCallable.java
+++ src/com/codari/arena5/arena/rules/TokenCallable.java
@@ -1,6 +1,6 @@
 package com.codari.arena5.arena.rules;
 
 public interface TokenCallable<T> {
-	public String store(T obj);
-	public T call(String string);
+	public String store(T obj) throws Exception;
+	public T call(String string) throws Exception;
 }
