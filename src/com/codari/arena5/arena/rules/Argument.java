6b6a05fcf1c4aeefd57dc73a466bd349f64d0670
Soren025 <Soren11001@gmail.com>
2014-01-17 16:07:43 -0600

    L

--- src/com/codari/arena5/arena/rules/Argument.java
+++ src/com/codari/arena5/arena/rules/Argument.java
@@ -1,5 +1,21 @@
 package com.codari.arena5.arena.rules;
 
-public enum Argument {
+import org.bukkit.Location;
 
+public enum Argument {
+	//-----Enumerations//
+	LOCATION(Location.class);
+	
+	//-----Fields-----//
+	private final Class<?> clazz;
+	
+	//-----Constructor-----//
+	private Argument(Class<?> clazz) {
+		this.clazz = clazz;
+	}
+	
+	//-----Methods-----//
+	public Class<?> clazz() {
+		return this.clazz;
+	}
 }
