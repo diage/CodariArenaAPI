41639389fa9846189b016fdb2de7aa7884376f4b
Mhenlo <mphils55@gmail.com>
2014-01-17 22:11:04 -0500

    primitives added to the Argument Enumeration

--- src/com/codari/arena5/arena/rules/Argument.java
+++ src/com/codari/arena5/arena/rules/Argument.java
@@ -3,7 +3,24 @@ package com.codari.arena5.arena.rules;
 import org.bukkit.Location;
 
 public enum Argument {
-	//-----Enumerations//
+	//-----Enumerations-----//
+	BOOLEAN_PRIMITIVE(boolean.class),
+	BOOLEAN_WRAPPER(Boolean.class),
+	BYTE_PRIMITIVE(byte.class),
+	BYTE_WRAPPER(Byte.class),
+	CHARACTER_PRIMITIVE(char.class),
+	CHARACTER_WRAPPER(Character.class),
+	DOUBLE_PRIMITIVE(double.class),
+	DOUBLE_WRAPPER(Double.class),
+	FLOAT_PRIMITIVE(float.class),
+	FLOAT_WRAPPER(Float.class),
+	INTEGER_PRIMITIVE(int.class),
+	INTEGER_WRAPPER(Integer.class),
+	LONG_PRIMITIVE(long.class),
+	LONG_WRAPPER(Long.class),
+	SHORT_PRIMITIVE(short.class),
+	SHORT_WRAPPER(Short.class),
+	STRING(String.class),
 	LOCATION(Location.class);
 	
 	//-----Fields-----//
