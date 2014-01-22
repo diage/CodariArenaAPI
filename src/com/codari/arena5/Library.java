bb1a209be3013a1552967cc7cfed389a46c78858
Mhenlo <mphils55@gmail.com>
2014-01-21 19:52:35 -0500

    Updated

--- src/com/codari/arena5/Library.java
+++ src/com/codari/arena5/Library.java
@@ -4,6 +4,7 @@ import com.codari.arena5.arena.rules.roledelegation.RoleDeclaration;
 import com.codari.arena5.arena.rules.timedaction.TimedAction;
 import com.codari.arena5.arena.rules.wincondition.WinCondition;
 import com.codari.arena5.objects.ArenaObject;
+import com.codari.arena5.players.skills.Skill;
 
 public interface Library {
 	public void registerWinCondition(Class<? extends WinCondition> clazz);
@@ -13,4 +14,6 @@ public interface Library {
 	public void registerRoleDeclaration(Class<? extends RoleDeclaration> clazz);
 	
 	public void registerArenaObject(Class<? extends ArenaObject> clazz);
+
+	public void registerSkill(Class<? extends Skill> clazz);
 }
