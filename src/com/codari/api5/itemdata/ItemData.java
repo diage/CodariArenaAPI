0bde2a91a187788fa2723ffe98543f4e0f6f80c3
Soren025 <Soren11001@gmail.com>
2014-02-02 21:13:07 -0600

    item data

--- src/com/codari/api5/itemdata/ItemData.java
+++ src/com/codari/api5/itemdata/ItemData.java
@@ -1,22 +1,6 @@
 package com.codari.api5.itemdata;
 
-import org.bukkit.inventory.ItemStack;
 
-public abstract class ItemData {
-	//-----Fields-----//
-	private final int id;
+public interface ItemData {
 	
-	//-----Constructor-----//
-	public ItemData(int id) {
-		this.id = id;
-	}
-	
-	//-----Methods-----//
-	public final int getId() {
-		return this.id;
-	}
-	
-	//-----Abstract Methods-----//
-	public abstract boolean canBeAppliedTo(ItemStack item);
-	public void onApplication(ItemStack item){}
 }
