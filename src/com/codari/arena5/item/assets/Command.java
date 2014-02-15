b353e84c2859f96c4b82f58d10011e2cf423d5c3
Mhenlo <mphils55@gmail.com>
2014-02-15 14:27:24 -0500

    update

--- src/com/codari/arena5/item/assets/Command.java
+++ src/com/codari/arena5/item/assets/Command.java
@@ -1,6 +1,8 @@
 package com.codari.arena5.item.assets;
 
+import com.codari.arena5.players.combatants.Combatant;
+
 
 public interface Command extends ItemAsset {
-	public void issue();
+	public void issue(Combatant combatant);
 }
