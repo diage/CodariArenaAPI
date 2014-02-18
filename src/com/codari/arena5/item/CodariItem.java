fbfc4663e581fa14108f66e9fe217575e3568c76
Soren025 <Soren11001@gmail.com>
2014-02-18 14:55:36 -0600

    c

--- src/com/codari/arena5/item/CodariItem.java
+++ src/com/codari/arena5/item/CodariItem.java
@@ -3,7 +3,8 @@ package com.codari.arena5.item;
 import com.codari.arena5.players.combatants.Combatant;
 
 public interface CodariItem {
-	
+	public String getSessionId();
+	public String getItemId();
 	public Combatant getCombatant();
 	
 }
