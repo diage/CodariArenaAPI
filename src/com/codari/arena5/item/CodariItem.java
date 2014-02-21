11df61b7e64e9e29af3be2302b7c080492c2cb59
Mhenlo <mphils55@gmail.com>
2014-02-20 22:05:58 -0500

    Add Item methods

--- src/com/codari/arena5/item/CodariItem.java
+++ src/com/codari/arena5/item/CodariItem.java
@@ -3,8 +3,13 @@ package com.codari.arena5.item;
 import com.codari.arena5.players.combatants.Combatant;
 
 public interface CodariItem {
-	public String getSessionId();
-	public String getItemId();
-	public Combatant getCombatant();
 	
+	public Combatant getCombatant();
+	public void attack(Combatant enemy);
+	public int getStackSize();
+	public void addStackSize(int size);
+	public void getAttributes();
+	public void levelUp();
+	public void addExp(int exp);
+	public void use();
 }
