a0105e87a4d8d18c7757657092eeb854ead8700c
Soren025 <Soren11001@gmail.com>
2014-02-21 21:10:51 -0600

    f

--- src/com/codari/api5/util/Modifier.java
+++
@@ -1,6 +0,0 @@
-package com.codari.api5.util;
-
-public interface Modifier {
-	public float getFixedValue();
-	public float getPercentage();
-}
--- src/com/codari/api5/util/Wrapper.java
+++
@@ -1,5 +0,0 @@
-package com.codari.api5.util;
-
-public interface Wrapper<T> {
-	public T getHandle();
-}
