d3389bac5e8050eb393ce835000da8fe27fe23f7
Soren025 <Soren11001@gmail.com>
2014-02-28 17:39:50 -0600

    assets

--- src/com/codari/api5/asset/Asset.java
+++ src/com/codari/api5/asset/Asset.java
@@ -2,21 +2,9 @@ package com.codari.api5.asset;
 
 public abstract class Asset {
 	//-----Fields-----//
-	private final AssetType type;
 	
 	//-----Constructor-----//
-	protected Asset() throws InvalidAssetException {
-		Class<? extends Asset> clazz = this.getClass();
-		this.type = AssetType.getType(clazz);
-		if (this.type == null) {
-			throw new InvalidAssetException(clazz);
-		}
-	}
+	protected Asset() {}
 	
 	//-----Methods-----//
-	
-	
-	public final AssetType getType() {
-		return this.type;
-	}
 }
